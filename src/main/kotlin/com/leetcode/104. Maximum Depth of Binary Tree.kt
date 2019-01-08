87ab414957a9a0450f61a8baa83e4da30aa0cb8c
DotSpy <knijaz@gmail.com>
2019-01-08 15:27:53 +0300

    104. Maximum Depth of Binary Tree.kt

---
+++ src/main/kotlin/com/leetcode/104. Maximum Depth of Binary Tree.kt
@@ -0,0 +1,19 @@
+package com.leetcode
+
+class MaximumDepthOfBinaryTree {
+
+  class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+      if (root == null) {
+        return 0
+      }
+      val left = maxDepth(root.left)
+      val right = maxDepth(root.right)
+      return if (left > right) {
+        left + 1
+      } else {
+        right + 1
+      }
+    }
+  }
+}
---
+++ src/test/kotlin/com/leetcode/MaximumDepthOfBinaryTreeTest.kt
@@ -0,0 +1,24 @@
+package com.leetcode
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class MaximumDepthOfBinaryTreeTest{
+
+  @Test
+  fun `leetcode example`(){
+    val solution = MaximumDepthOfBinaryTree.Solution()
+    val tree = TreeNode().also { root ->
+      root.`val` = 3
+      root.left = TreeNode().apply {
+        `val` = 9
+      }
+      root.right = TreeNode().apply {
+        `val` = 20
+        left = TreeNode().apply { `val` = 15 }
+        right = TreeNode().apply { `val` = 7 }
+      }
+    }
+    Assertions.assertEquals(3, solution.maxDepth(tree))
+  }
+}
