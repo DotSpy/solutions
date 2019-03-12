ad36744a189393d3863e479a141b0d07ecffc251
DotSpy <knijaz@gmail.com>
2019-03-12 14:41:24 +0300

    226. Invert Binary Tree.kt

---
+++ src/main/kotlin/com/leetcode/226. Invert Binary Tree.kt
@@ -0,0 +1,44 @@
+package com.leetcode
+
+/**
+Invert a binary tree.
+
+Example:
+
+Input:
+
+4
+/   \
+2     7
+/ \   / \
+1   3 6   9
+Output:
+
+4
+/   \
+7     2
+/ \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ */
+class InvertBinaryTree {
+
+  class Solution {
+
+    fun invertTree(root: TreeNode?): TreeNode? {
+      val left = root?.left
+      root?.left = root?.right
+      root?.right = left
+      if (root?.left != null) {
+        invertTree(root.left)
+      }
+      if (root?.right != null) {
+        invertTree(root.right)
+      }
+      return root
+    }
+  }
+}
---
+++ src/test/kotlin/com/leetcode/InvertBinaryTreeTest.kt
@@ -0,0 +1,39 @@
+package com.leetcode
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class InvertBinaryTreeTest {
+
+  @Test
+  fun `base input leetcode`() {
+    val solution = InvertBinaryTree.Solution()
+    val tree = TreeNode().also { root ->
+      root.`val` = 4
+      root.left = TreeNode().apply {
+        `val` = 2
+        left = TreeNode().apply { `val` = 1 }
+        right = TreeNode().apply { `val` = 3 }
+      }
+      root.right = TreeNode().apply {
+        `val` = 7
+        left = TreeNode().apply { `val` = 6 }
+        right = TreeNode().apply { `val` = 9 }
+      }
+    }
+    val expectedTree = TreeNode().also { root ->
+      root.`val` = 4
+      root.left = TreeNode().apply {
+        `val` = 7
+        left = TreeNode().apply { `val` = 9 }
+        right = TreeNode().apply { `val` = 6 }
+      }
+      root.right = TreeNode().apply {
+        `val` = 2
+        left = TreeNode().apply { `val` = 3 }
+        right = TreeNode().apply { `val` = 1 }
+      }
+    }
+    Assertions.assertEquals(expectedTree, solution.invertTree(tree))
+  }
+}
