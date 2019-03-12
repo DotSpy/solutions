456a9359f1c960548a565bb50e5963f1b1373c23
DotSpy <knijaz@gmail.com>
2019-03-12 19:12:16 +0300

    538. Convert BST to Greater Tree.kt

---
+++ src/test/kotlin/com/leetcode/ConvertBSTtoGreaterTreeTest.kt
@@ -0,0 +1,47 @@
+package com.leetcode
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ConvertBSTtoGreaterTreeTest {
+
+  @Test
+  fun `leetcode base input`() {
+    val solution = ConvertBSTtoGreaterTree.Solution()
+    val tree = TreeNode().also { root ->
+      root.`val` = 5
+      root.left = TreeNode().apply {
+        `val` = 2
+      }
+      root.right = TreeNode().apply {
+        `val` = 13
+      }
+    }
+    val expectedTree = TreeNode().apply {
+      `val` = 18
+      left = TreeNode().apply { `val` = 20 }
+      right = TreeNode().apply { `val` = 13 }
+    }
+    Assertions.assertEquals(expectedTree, solution.convertBST(tree))
+  }
+
+  @Test
+  fun `leetcode example 1`() {
+    val solution = ConvertBSTtoGreaterTree.Solution()
+    val tree = TreeNode().also { root ->
+      root.`val` = 2
+      root.left = TreeNode().apply {
+        `val` = 1
+      }
+      root.right = TreeNode().apply {
+        `val` = 3
+      }
+    }
+    val expectedTree = TreeNode().apply {
+      `val` = 5
+      left = TreeNode().apply { `val` = 6 }
+      right = TreeNode().apply { `val` = 3 }
+    }
+    Assertions.assertEquals(expectedTree, solution.convertBST(tree))
+  }
+}
